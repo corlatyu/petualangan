cb52ed05ce43765ac2e83eb3ce221286db550601
Franco <a21a17642@educacion.edu.do>
2024-06-07 13:52:53 +0700

    Update index.js

--- index.js
+++ index.js
@@ -332,21 +332,21 @@ let levels = {
       // Sets up mobile traps, saws
       traps = [
         new Saw({
-          imageSrc: "./img/traps/onSaw.png",
+          imageSrc: "./img/traps/OnSaw.png",
           frameRate: 8,
           animations: {},
           x: 387.0,
           y: 214.0,
         }),
         new Saw({
-          imageSrc: "./img/traps/onSaw.png",
+          imageSrc: "./img/traps/OnSaw.png",
           frameRate: 8,
           animations: {},
           x: 515.0,
           y: 214.0,
         }),
         new Saw({
-          imageSrc: "./img/traps/onSaw.png",
+          imageSrc: "./img/traps/OnSaw.png",
           frameRate: 8,
           animations: {},
           x: 643.0,
